12f473a0f0e3f5653f45e74149572a62232a87da
Jelena <jelena.durovic@powerhrg.com>
2021-06-01 07:58:34 -0400

    Completed assigment

---
+++ js/challenge.js
@@ -0,0 +1,60 @@
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+let n = document.querySelector("#counter")
+
+
+let c = parseInt(n.innerText)
+
+setInterval(function() {
+    n.innerText = c ++}, 1000)
+
+
+
+
+    let incr = document.querySelector("#plus")
+    let decr = document.querySelector("#minus")
+
+incr.addEventListener("click", function(){
+   c = c+1
+    n.innerText = c 
+
+
+})
+
+decr.addEventListener("click", function() {
+    c = c-1
+n.innerText = c 
+
+})
+
+
+const heart = document.querySelector("#heart")
+
+heart.addEventListener("click", function(){
+    let n = document.querySelector("#counter")
+    let c = parseInt(n.innerText)
+     let mainClass = document.querySelector(".likes")
+
+
+const array = []
+if(array.includes(c)){
+ array.push(c)
+ const times = array.filter( (a) => a === c)
+ const likeElement = document.getElementById(`${c}`)
+ likeElement.innerHTML = `${c} has been liked ${times.length} times`
+
+} else {
+    const likeElement = document.createElement("li")
+    likeElement.id = `${c}`
+    likeElement.innerHTML = `${c} has been liked 1 time`
+    array.push(c)
+    mainClass.append(likeElement)
+}
+
+
+})
+
+
+})
